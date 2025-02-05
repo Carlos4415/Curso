22731352ee9588e8ace2357fd2142c1161e5d329
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-05 18:19:02 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Este projeto demonstra como criar uma variável em C#, atribuir a ela o nome de um curso 
+// e exibir o conteúdo dessa variável no console. Ele é um exemplo básico para introduzir 
+// conceitos de declaração de variáveis, atribuição de valores e saída de dados no console.
+string curso = "C# - Criando sua primeira aplicação";
+
+Console.WriteLine(curso);
+Console.WriteLine("Daniel Portugal e Gui Lima");
